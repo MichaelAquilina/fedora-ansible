f16fe3e5a274376b85cbc65207feccf14741e3c0
Michael Aquilina <michaelaquilina@gmail.com>
2024-12-06 15:27:00 +0000

    Use lazy instead of packer

--- roles/nvim/files/init.lua
+++ roles/nvim/files/init.lua
@@ -14,192 +14,34 @@ vim.g.nvim_tree_hijack_netrw = 0
 vim.g.mapleader = " ";
 
 -- Python configuration
-vim.python_host_prog = '/usr/bin/python';
-vim.python3_host_prog = '/usr/bin/python3';
-
-local packer = require('packer');
-packer.startup(function(use)
-  use('wbthomason/packer.nvim');
-
-  -- colorscheme
-  use({'folke/tokyonight.nvim', branch = 'main' });
-
-  -- Functionality
-  use({'wellle/targets.vim'});
-  use({'kyazdani42/nvim-tree.lua'});
-  use('norcalli/nvim-colorizer.lua');
-  use('lukas-reineke/indent-blankline.nvim');
-  use({'akinsho/bufferline.nvim', branch = 'main'});
-
-  use({
-    'Pocco81/auto-save.nvim',
-    config = function()
-      require("auto-save").setup {
-        -- your config goes here
-	-- or just leave it empty :)
-      }
-    end
-  })
-
-  -- Telescope
-  use({
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  })
-  use({'nvim-telescope/telescope-fzf-native.nvim', run = 'make' })
-
-  -- Language support
-  use({'towolf/vim-helm'});
-
-  -- LSP
-  use({'VonHeikemen/lsp-zero.nvim'});
-  use({'soulis-1256/eagle.nvim'});
-  use({'neovim/nvim-lspconfig'});
-  use ({
-    "nvimdev/lspsaga.nvim",
-    after = "nvim-lspconfig",
-    config = function()
-        require("lspsaga").setup({})
-    end,
-})
-
-  use('ray-x/lsp_signature.nvim');
-  use('williamboman/mason.nvim');
-  use('williamboman/mason-lspconfig.nvim')
-
-  use('hrsh7th/nvim-cmp');
-  use('hrsh7th/cmp-nvim-lsp');
-  use('L3MON4D3/LuaSnip');
-  use('saadparwaiz1/cmp_luasnip');
-  use('onsails/lspkind-nvim');
-
-  -- Treesitter
-  use({'nvim-treesitter/nvim-treesitter'});
-  use({'nvim-treesitter/nvim-treesitter-textobjects'});
-  use({'windwp/nvim-ts-autotag'});
-
-  -- Git
-  use('tpope/vim-fugitive');
-  use('tpope/vim-rhubarb');
-
-  use('tpope/vim-sleuth');  -- Automatic tab expand configuration
-  use('tpope/vim-commentary');  -- Comment out blocks of code
-  use({
-    'kylechui/nvim-surround',
-    tag = '*',
-    config = function()
-        require("nvim-surround").setup({
-        })
-    end
-  });  -- change surrounding elements
-end);
-
-
-vim.g.tokyonight_style = 'night';
-vim.cmd("colorscheme tokyonight");
-
--- Bufferline
-
-require("bufferline").setup{}
+vim.g.python_host_prog = '/usr/bin/python';
+vim.g.python3_host_prog = '/usr/bin/python3';
 
 vim.o.spell = true;
 
--- Telescope
-local telescope = require('telescope')
-telescope.setup({
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-    }
-  },
-pickers = {
-    buffers = {
-      sort_mru = true,
-      sort_lastused = true,
-    }
-  }
-});
-telescope.load_extension("fzf");
+-- Miscellaneous options
 
--- Colorizer
-
-require('colorizer').setup()
-
---- Eagle
-
-require('eagle').setup({})
 vim.o.mousemoveevent = true
+vim.o.list = true;
+vim.o.completeopt = "menuone,noinsert,noselect";
+vim.o.encoding = "utf-8";
+vim.o.emoji = true;
+vim.o.inccommand =  "nosplit"  -- Enable previewing of %s//
+vim.o.mouse = "a"
+vim.o.wrap = false;
+vim.o.number = true;
+vim.o.hidden = true;
+vim.o.backup = false;
+vim.o.writebackup = false;
+vim.o.swapfile = false;
+vim.o.cursorline = true;
+vim.o.cursorcolumn = true;
+vim.o.expandtab = true;
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
 
--- Treesitter configuration
+require('config.lazy');
 
-require('nvim-tree').setup()
-
-
-local treesitter = require('nvim-treesitter.configs')
-treesitter.setup({
-  autotag = { enable = true },
-  ensure_installed = "all",
-  highlight = {
-    enable = true
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
-      },
-    },
-  },
-})
-
--- Lsp configuration
-local lsp_zero = require('lsp-zero')
-
-lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  -- Replace the language servers listed here
-  -- with the ones you want to install
-  ensure_installed = {},
-  handlers = {
-    lsp_zero.default_setup,
-  }
-})
-
-local cmp = require('cmp');
-
-cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-  }),
-  -- sources for autocompletion
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" }, -- LSP
-    { name = "buffer" }, -- text within the current buffer
-    { name = "path" }, -- file system paths
-  }),
-})
-
--- NvimTree
-vim.g.nvim_tree_ignore = { '.git' }
-vim.g.nvim_tree_gitignore = 1
-vim.g.nvim_tree_show_icons = {
-    git = 0,
-    folders= 1,
-    files = 0,
-    folder_arrows = 1,
-};
 
 vim.keymap.set('n', '<leader>ev', ':edit $MYVIMRC<cr>')
 vim.keymap.set('n', '<leader>el', ':edit ~/.config/nvim/init.lua<cr>')
@@ -208,6 +50,8 @@ vim.keymap.set('n', '<leader>ez', ':edit ~/.zshrc<cr>')
 vim.keymap.set('n', '<leader>es', ':edit ~/.config/sway/config<cr>')
 vim.keymap.set('n', '<leader>ek', ':edit ~/.config/kitty/kitty.conf<cr>')
 
+vim.keymap.set('n', '<leader>ff', ':lua vim.lsp.buf.format()<cr>')
+
 vim.keymap.set('n', '<c-p>', ':Telescope find_files find_command=rg,--ignore,-g,!.git/,--hidden,--files<cr>')
 vim.keymap.set('n', '<c-b>', ':Telescope buffers<cr>')
 vim.keymap.set('n', '<c-k>', ':Telescope live_grep<cr>')
@@ -215,10 +59,10 @@ vim.keymap.set('n', '<c-k>', ':Telescope live_grep<cr>')
 vim.keymap.set('v', 'cp', '"+y')
 vim.keymap.set('n', 'vp', '"+p')
 
-vim.keymap.set('n', 'Q', '<nop>') -- Disable Ex mode
-vim.keymap.set('n', '<c-s>', '<nop>') -- Disable stop redraw
+vim.keymap.set('n', 'Q', '<nop>')              -- Disable Ex mode
+vim.keymap.set('n', '<c-s>', '<nop>')          -- Disable stop redraw
 
-vim.keymap.set('n', '<esc>',':let @/=""<cr>') -- Cancel current search
+vim.keymap.set('n', '<esc>', ':let @/=""<cr>') -- Cancel current search
 
 -- Quickly move lines up and down
 vim.keymap.set('n', '-', 'ddp')
@@ -241,23 +85,3 @@ vim.keymap.set('n', 'gu', ':Telescope lsp_references<CR>')
 vim.keymap.set('v', '<leader>gb', ':GBrowse<cr>')
 
 vim.keymap.set('n', '<leader>/', ':NvimTreeToggle<cr>')
-
--- Miscellaneous options
-
-vim.o.list = true;
-vim.o.completeopt = "menuone,noinsert,noselect";
-vim.o.encoding = "utf-8";
-vim.o.emoji = true;
-vim.o.inccommand =  "nosplit"  -- Enable previewing of %s//
-vim.o.mouse = "a"
-vim.o.wrap = false;
-vim.o.number = true;
-vim.o.hidden = true;
-vim.o.backup = false;
-vim.o.writebackup = false;
-vim.o.swapfile = false;
-vim.o.cursorline = true;
-vim.o.cursorcolumn = true;
-vim.o.expandtab = true;
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
---
+++ roles/nvim/files/lua/config/lazy.lua
@@ -0,0 +1,205 @@
+local vim = vim
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim";
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath);
+
+require("lazy").setup({
+    -- colorscheme
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd([[colorscheme tokyonight]]);
+            vim.g.tokyonight_style = 'night';
+        end,
+    },
+
+    -- Functionality
+    { 'wellle/targets.vim' },
+    { 'kyazdani42/nvim-tree.lua' },
+    { 'norcalli/nvim-colorizer.lua' },
+    { 'lukas-reineke/indent-blankline.nvim' },
+    { 'akinsho/bufferline.nvim', branch = 'main' },
+
+    {
+        'Pocco81/auto-save.nvim',
+        config = function()
+            require("auto-save").setup {}
+        end
+    },
+
+    -- Telescope
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = { { 'nvim-lua/plenary.nvim' } }
+    },
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+    },
+
+    -- Language support
+    { 'towolf/vim-helm' },
+
+    -- LSP
+    { 'VonHeikemen/lsp-zero.nvim' },
+    { 'soulis-1256/eagle.nvim' },
+    { 'neovim/nvim-lspconfig' },
+    {
+        "nvimdev/lspsaga.nvim",
+        dependencies = { 'neovim/nvim-lspconfig' },
+        config = function()
+            require("lspsaga").setup({
+                lightbulb = { enable = false }
+            })
+        end,
+    },
+
+    { 'ray-x/lsp_signature.nvim' },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'L3MON4D3/LuaSnip' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { 'onsails/lspkind-nvim' },
+
+    -- Treesitter
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+    },
+    { 'nvim-treesitter/nvim-treesitter-textobjects' },
+    { 'windwp/nvim-ts-autotag' },
+
+    -- Git
+    { 'tpope/vim-fugitive' },
+    { 'tpope/vim-rhubarb' },
+
+    { 'tpope/vim-sleuth' },     -- Automatic tab expand configuration
+    { 'tpope/vim-commentary' }, -- Comment out blocks of code
+    {
+        'kylechui/nvim-surround',
+        version = '*',
+        config = function()
+            require("nvim-surround").setup({
+            })
+        end
+    },
+
+
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "tokyonight" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+});
+
+require("bufferline").setup {}
+
+local telescope = require('telescope')
+telescope.setup({
+    extensions = {
+        fzf = {
+            fuzzy = true,                   -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true,    -- override the file sorter
+            case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+        }
+    },
+    pickers = {
+        buffers = {
+            sort_mru = true,
+            sort_lastused = true,
+        }
+    }
+});
+telescope.load_extension("fzf");
+
+require('colorizer').setup()
+
+require('eagle').setup({})
+
+require('nvim-tree').setup()
+
+require('nvim-treesitter.configs').setup({
+    autotag = { enable = true },
+    ensure_installed = "all",
+    ignore_install = { "hoon" },
+    highlight = {
+        enable = true
+    },
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
+            },
+        },
+    },
+})
+
+-- Lsp configuration
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+    lsp_zero.default_keymaps({ buffer = bufnr })
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    -- Replace the language servers listed here
+    -- with the ones you want to install
+    automatic_installation = true,
+    ensure_installed = {
+        'pylsp',
+        'lua_ls',
+        'ts_ls',
+        'gopls',
+        'clangd',
+    },
+    handlers = {
+        lsp_zero.default_setup,
+    }
+})
+
+local cmp = require('cmp');
+cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    }),
+    -- sources for autocompletion
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" }, -- LSP
+        { name = "buffer" },   -- text within the current buffer
+        { name = "path" },     -- file system paths
+    }),
+})
+
+-- NvimTree
+vim.g.nvim_tree_ignore = { '.git' }
+vim.g.nvim_tree_gitignore = 1
+vim.g.nvim_tree_show_icons = {
+    git = 0,
+    folders = 1,
+    files = 0,
+    folder_arrows = 1,
+};
